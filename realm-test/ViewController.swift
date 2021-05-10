59496332ac504e3f75fef8056c3679818d1b89c2
Yevhen Pyvovarov <evgenypyvovarov@gmail.com>
2021-05-10 12:31:29 +0300

    Added empty project

---
+++ realm-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  realm-test
+//
+//  Created by Yevhen Pyvovarov on 10.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
